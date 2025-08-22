7760a518857eb14c18eba28d99a74dca9553a1d5
camillyvitoria380 <00001135389202SP@aluno.educacao.sp.gov.br>
2025-08-22 07:31:33 -0300

    S17 A2

---
+++ distancia.py
@@ -0,0 +1,4 @@
+distanciax = float(input("digite a distancia [km]:"))
+total_combustively = float(input("digite o total de combustivel gasto [litros]: "))
+consumo = distanciax/total_combustively
+    print(f"o consumo medio e de {consumo.3f}km/l")
