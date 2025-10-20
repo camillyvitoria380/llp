b65169f53e531d575776112ff37e1515a7ec79de
camillyvitoria380 <00001135389202sp@aluno.educacao.sp.gov.br>
2025-10-20 08:13:39 -0300

    teste

--- S25A3.py
+++ S25A3.py
@@ -11,5 +11,33 @@ def reserva_assento(cinema,linha,coluna):
     posiçao_l=-1
     posiçao_coluna=-1
 
+def cancelar_assento(cinema,linha,coluna):
+    posiçao_linha = linha - 1
+    posiçao_coluna = coluna - 1
+    if 0 <= posiçao_linha < 5 and  0 <= posiçao_coluna < 8:
+        if cinema[posiçao_linha][posiçao_coluna] == 1:
+            cinema[posiçao_linha][posiçao_linha] = 0
+            print(f"\na cadeira [{linha}][{coluna}]foi cancelada")
+        else:
+            print(f"a cadeira[{linha}][{coluna}]continua livre")
+    else:
+        print(f"a cadeira[{linha}][{coluna}]não e válida")
+
+def exibir(cinema,linha,coluna):
+    for i in range(linha):
+        for j in range(coluna):
+            print(cinema[i][j], end=' ')
+        print()
+
+def execucao(cinema,dim_linha,dim_coluna, linha, coluna):
+    print("cinema sem reservas")
+    exibir(cinema, dim_linha, dim_coluna)
+    reserva_assento(cinema, linha, coluna)
+    print("cinema depois da reserva")
+    exibir(cinema, dim_linha, dim_coluna)
+
+if __name__=='__main__':
+    execucao(cinema, 5,8, 1,3)
+        
 
 
